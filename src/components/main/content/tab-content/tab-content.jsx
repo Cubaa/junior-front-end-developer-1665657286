62201f2cb8ae6072e6201db0201dc71001c4bc27
Cubaa <jakub29-11@wp.pl>
2022-10-16 01:24:58 +0200

    add dynamic render content when student change task in your task

--- src/components/main/content/tab-content/tab-content.jsx
+++ src/components/main/content/tab-content/tab-content.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { getBusinessContexts } from "../../../../utils/get-business-contexts";
 import { ContextsList } from "./context-list/context-list";
 import { ContextsContent } from "./contexts-content/contexts-content";
 import { TabContentComponent } from "./tab-content.styled";
 
 export const TabContent = () => {
-  const businessContexts = getBusinessContexts("Connect Admin with Frontend");
-
   return (
     <TabContentComponent>
-      <ContextsList businessContexts={businessContexts} />
-      <ContextsContent businessContexts={businessContexts} />
+      <ContextsList />
+      <ContextsContent />
     </TabContentComponent>
   );
 };
--- src/context/business-context.js
+++ src/context/business-context.js
@@ -3,4 +3,6 @@ import { createContext } from "react";
 export const BusinessContext = createContext({
   businessContextId: 0,
   setBusinessContextId: () => {},
+  businessContexts: [],
+  setBusinessContexts: () => {},
 });
